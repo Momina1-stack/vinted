e7959ff8c692d52901d58e3c1ed812f4f0b1a759
momina saleem <mominarare@gmail.com>
2026-01-26 12:42:57 +0500

    about-us

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { SubItems } from './pages/sub-items/sub-items';
 import { Login } from './login/login';
 import { ImgUpload } from './img-upload/img-upload';
 import { AboutSubsection } from './about-subsection/about-subsection';
+import { AboutUs } from './about-us/about-us';
 
 export const routes: Routes = [
 
@@ -17,6 +18,8 @@ export const routes: Routes = [
       { path: 'login', component: Login },
       { path: 'items', component: BagItems },
       { path: 'about-subsection', component: AboutSubsection },
+      { path: 'about-us', component: AboutUs },
+
 
 
 
