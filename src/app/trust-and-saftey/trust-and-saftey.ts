9f96f525b6a3ceb0b2f4d74f4d8987878dece572
momina saleem <mominarare@gmail.com>
2026-02-16 15:34:30 +0500

    login-form

--- src/app/trust-and-saftey/trust-and-saftey.ts
+++ src/app/trust-and-saftey/trust-and-saftey.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-trust-and-saftey',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './trust-and-saftey.html',
   styleUrl: './trust-and-saftey.css',
 })
